4d73d455381ed6a54167a60683de33049a17a698
Harshini A <harshinidurai98@gmail.com>
2018-01-18 10:36:19 +0000

    Update min.c

--- min.c
+++ min.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 int main()
 {
-int a[10],i,j,n,temp;
-scanf("%d",&n);
-for(i=0;i<n;i++)
+int a[20],i,j,temp,num;
+scanf("%d",&num);
+for(i=0;i<num;i++)
 scanf("%d",&a[i]);
-for(i=0;i<n;i++)
-{
-for(j=i+1;j<n;j++)
-{
-if(a[i]>a[j])
-{
-temp=a[i];
-a[i]=a[j];
-a[j]=temp;
-}
-}
-}
-printf("%d",a[n-1]);
-return 0;
+ for(i=0;i<num;i++)
+ {
+   for(j=i+1;j<num;j++)
+   {
+     if(a[i]>a[j])
+     {
+       temp=a[i];
+       a[i]=a[j];
+       a[j]=temp;
+     }
+   }
+ }
+  printf("min=%d",a[0]);
+  return 0;
 }
