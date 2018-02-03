edd332bc518f19f67756041c914de71e102527f7
Harshini A <harshinidurai98@gmail.com>
2018-02-03 06:50:11 +0530

    Update subtract time.c

--- subtract time.c
+++ subtract time.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 int main()
 {
-	int hr1,min1,hr2,min2;
+	int hr1,min1,hr2,min2,hr,min;
 	scanf("%d%d",&hr1,&min1);
 	scanf("%d%d",&hr2,&min2);
-printf("%d:%d",hr2-hr1,min2-min1);
+	hr=(hr2-hr1)*60;
+	min=min2-min1;
+	min+=hr;
+	printf("%d:%d\n",min/60,min%60);
 	return 0;
 }
